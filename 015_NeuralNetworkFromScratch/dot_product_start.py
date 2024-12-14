3d964ff1f4cbf5bb521c6967e12b964adc7116af
Neeraj Goel <negoel@Neerajs-MacBook-Pro.local>
2024-12-13 20:16:20 -0800

    Solving question for dot_product_start

--- 015_NeuralNetworkFromScratch/dot_product_start.py
+++ 015_NeuralNetworkFromScratch/dot_product_start.py
@@ -6,3 +6,9 @@ w1 = [2, 3]
 w2 = [0.4, 1.8]
 
 # %% Question: which weight is more similar to input data X
+x_dot_w1 = np.dot(X, w1)
+x_dot_w2 = np.dot(X, w2)
+if x_dot_w1 > x_dot_w2:
+    print('w1 is more similar to X')
+else:
+    print('w2 is more similar to X')
